e1b11fb283e69646f61080e95d6c51e14d62a5cd
Itamar Golan <itamarg@folloze.com>
2020-03-04 14:40:57 +0200

    changed named path for sessions#new

--- lib/devise/rails/routes.rb
+++ lib/devise/rails/routes.rb
@@ -342,7 +342,7 @@ module ActionDispatch::Routing
 
       def devise_session(mapping, controllers) #:nodoc:
         resource :session, :only => [], :controller => controllers[:sessions], :path => "" do
-          get   :new,     :path => mapping.path_names[:sign_in],  :as => "new"
+          get   :new,     :path => mapping.path_names[:sign_in],  :as => "devise_new"
           post  :create,  :path => mapping.path_names[:sign_in]
           match :destroy, :path => mapping.path_names[:sign_out], :as => "destroy", :via => mapping.sign_out_via
         end
